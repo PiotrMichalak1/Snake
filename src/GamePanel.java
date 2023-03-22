1fcfece77b4b2182d8126ac091f84e0255524fa0
PiotrMichalak1 <128514914+PiotrMichalak1@users.noreply.github.com>
2023-03-22 14:16:08 +0100

    Added Draw Methods

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -11,13 +11,13 @@ public class GamePanel extends JPanel implements ActionListener {
     static final int SCREEN_WIDTH = 600;
     static final int SCREEN_HEIGHT = 600;
     static final int UNIT_SIZE = 25;
-    static final int GAME_UNITS = (SCREEN_HEIGHT*SCREEN_WIDTH)/UNIT_SIZE;
+    static final int GAME_UNITS = (SCREEN_HEIGHT * SCREEN_WIDTH) / (UNIT_SIZE * UNIT_SIZE);
 
     static final int DELAY = 75;
 
     final int x[] = new int[GAME_UNITS];
     final int y[] = new int[GAME_UNITS];
-    int bodyParts =6;
+    int bodyParts = 6;
     int applesEaten;
     int appleX;
     int appleY;
@@ -27,64 +27,115 @@ public class GamePanel extends JPanel implements ActionListener {
     Random random;
 
 
-    GamePanel(){
+    GamePanel() {
         random = new Random();
-        this.setPreferredSize(new Dimension(SCREEN_WIDTH,SCREEN_WIDTH));
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_WIDTH));
         this.setBackground(Color.black);
         this.setFocusable(true);
         this.addKeyListener(new MyKeyAdapter());
         startGame();
-/*        newApple();
-        repaint();*/
     }
 
-    public void startGame(){
+    public void startGame() {
         newApple();
         running = true;
         timer = new Timer(DELAY, this);
         timer.start();
     }
+
     @Override
-    public void paintComponent(Graphics g){
+    public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        draw(g);
+        updateFrame(g);
     }
-    public void draw(Graphics g){
-        for(int i =0; i<SCREEN_HEIGHT/UNIT_SIZE;i++){
-            g.drawLine(i*UNIT_SIZE,0,i*UNIT_SIZE,SCREEN_HEIGHT);
+
+    public void updateFrame(Graphics g) {
+
+        drawApple(g);
+        drawGrid(g);
+        drawSnake(g);
+    }
+
+    public void drawSnake(Graphics g){
+        for (int i = 0; i < bodyParts; i++) {
+            if (i == 0) {
+                g.setColor(Color.green);
+                g.fillRect(x[i],y[i],UNIT_SIZE,UNIT_SIZE);
+            } else {
+                g.setColor(new Color(45,180,0));
+                g.fillRect(x[i],y[i],UNIT_SIZE,UNIT_SIZE);
+            }
         }
-        for(int i =0; i<SCREEN_WIDTH/UNIT_SIZE;i++){
-            g.drawLine(0,i*UNIT_SIZE,SCREEN_HEIGHT,i*UNIT_SIZE);
+    }
+
+    public void drawGrid(Graphics g){
+        g.setColor(Color.darkGray);
+        for (int i = 0; i < SCREEN_HEIGHT / UNIT_SIZE; i++) {
+            g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT);
         }
-        /*g.setColor(Color.red);
-        g.drawOval(appleX*UNIT_SIZE,appleY*UNIT_SIZE,UNIT_SIZE,UNIT_SIZE);*/
-    }
-    public void newApple(){
-        appleX = random.nextInt(SCREEN_WIDTH/UNIT_SIZE);
-        appleY = random.nextInt(SCREEN_HEIGHT/UNIT_SIZE);
+        for (int i = 0; i < SCREEN_WIDTH / UNIT_SIZE; i++) {
+            g.drawLine(0, i * UNIT_SIZE, SCREEN_HEIGHT, i * UNIT_SIZE);
+        }
     }
 
-    public void move(){
+    public void drawApple(Graphics g) {
+        g.setColor(Color.red);
+        g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+    }
+
+    public void newApple() {
+        appleX = random.nextInt((int) (SCREEN_WIDTH / UNIT_SIZE)) * UNIT_SIZE;
+        appleY = random.nextInt((int) (SCREEN_HEIGHT / UNIT_SIZE)) * UNIT_SIZE;
+    }
+
+    public void move() {
+        for (int i = bodyParts; i > 0; i--) {
+            x[i] = x[i - 1];
+            y[i] = y[i - 1];
+        }
+        switch (direction) {
+            case 'U':
+                y[0] = y[0] - UNIT_SIZE;
+                break;
+            case 'D':
+                y[0] = y[0] + UNIT_SIZE;
+                break;
+            case 'L':
+                x[0] = x[0] - UNIT_SIZE;
+                break;
+            case 'R':
+                x[0] = x[0] + UNIT_SIZE;
+                break;
+
+        }
 
     }
-    public void checkApple(){
+
+    public void checkApple() {
 
     }
-    public void checkCollisions(){
+
+    public void checkCollisions() {
 
     }
-    public void gameOver(Graphics g){
+
+    public void gameOver(Graphics g) {
 
     }
+
     /*Well add some methods here*/
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        if (running) {
+            move();
+            checkApple();
+            checkCollisions();
+        }
     }
 
     public class MyKeyAdapter extends KeyAdapter {
         @Override
-        public void keyPressed(KeyEvent e){
+        public void keyPressed(KeyEvent e) {
 
         }
 
